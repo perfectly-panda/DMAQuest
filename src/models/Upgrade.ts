a8eac82ebbbbf79b2828c57039257d6bf2e90ebd
PerfectlyPanda <samarasoucy@gmail.com>
2022-11-21 10:24:01 -0500

    add upgrade functions

--- src/models/Upgrade.ts
+++ src/models/Upgrade.ts
@@ -1,43 +1,54 @@
+import { useResourceStore } from "@/stores/ResourceStore"
 import type { IStoryFlag } from "@/types/IStore"
 
 export interface newUpgrade {
   id: string,
   name: string,
   description: string,
+  available?: boolean,
   cost: number,
-  multiplier: number,
-  purchased: boolean,
+  multiplier?: number,
+  purchased?: number,
+  max?: number,
   costResource: IStoryFlag,
-  improvedResource: IStoryFlag,
+  apply?: Function,
 }
+
+export type ImprovementType = "perSecond" | "staticValue" | "max" | "min"
 
 export class Upgrade {
   id: string
   name: string
   description: string
+  available: boolean
   cost: number
   multiplier: number
-  purchased: boolean
+  purchased: number
+  max: number
   costResource: IStoryFlag
-  improvedResource: IStoryFlag
+  apply: Function
   
   constructor({
     id,
     name,
     description,
+    available = false,
     cost,
-    multiplier,
-    purchased = false,
+    multiplier = 1.1,
+    purchased = 0,
+    max = 10,
     costResource,
-    improvedResource,
+    apply = () => {},
   } : newUpgrade) {
     this.id = id
     this.name = name
     this.description = description
+    this.available = available
     this.cost = cost
     this.multiplier = multiplier
     this.purchased = purchased
+    this.max = max
     this.costResource = costResource
-    this.improvedResource = improvedResource
+    this.apply = apply
   }
 }
